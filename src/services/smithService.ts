175de0d335f06b947f4ef3862f8efa14e2c6c547
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-05 16:29:02 -0300

    Cria service para registrar produto

--- src/services/smithService.ts
+++ src/services/smithService.ts
@@ -2,3 +2,14 @@ import connection from '../models/connection';
 import SmithModel from '../models/smithModel';
 import IProduct from '../interfaces/productInterface';
 import IUser from '../interfaces/userInterface';
+
+export default class SmithService {
+  public model: SmithModel;
+
+
+  public async registerProducts(product: IProduct): Promise<IProduct> {
+    const registerProdResponse = await this.model.registerProducts(product);
+    return registerProdResponse;
+  }
+
+}
