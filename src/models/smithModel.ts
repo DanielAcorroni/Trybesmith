7acc495add08fd8bb1f2120f728ee34bf4216ce0
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-05 16:27:11 -0300

    Cria model para registrar produto

--- src/models/smithModel.ts
+++ src/models/smithModel.ts
@@ -2,3 +2,21 @@ import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import IProduct from '../interfaces/productInterface';
 import IUser from '../interfaces/userInterface';
 
+export default class SmithModel {
+  private connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+
+  public async registerProducts(product: IProduct): Promise<IProduct> {
+    const { name, amount } = product;
+    const [response] = await this.connection
+      .execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    return { id: response.insertId, ...product };
+  }
+}
