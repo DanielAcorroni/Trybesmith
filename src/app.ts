efbafe8256c61e92c25ba09e495d40ef9effdce2
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-05 16:34:11 -0300

    utiliza as rotas criadas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import productRoutes from './routes/productRoutes';
+import userRoutes from './routes/userRoutes';
+import orderRoutes from './routes/orderRoutes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(productRoutes);
+
+app.use(userRoutes);
+
+app.use(orderRoutes);
 
 export default app;
