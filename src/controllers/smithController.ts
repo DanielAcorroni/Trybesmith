b9b38d1a63a66befaabb3cb592eaeb5297ea767c
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-05 16:31:21 -0300

    Cria controller para registrar produto

--- src/controllers/smithController.ts
+++ src/controllers/smithController.ts
@@ -1,3 +1,14 @@
 import { Request, Response } from 'express';
 import generateJWT from '../auth/generateJWT';
 import SmithService from '../services/smithService';
+
+export default class SmithController {
+  constructor(private service = new SmithService()) { }
+
+
+  public registerProducts = async (req: Request, res: Response) => {
+    const product = req.body;
+    const registerProdResponse = await this.service.registerProducts(product);
+    return res.status(201).json(registerProdResponse);
+  };
+}
