488fc746f2af59dbcacda498a90316cc7bb09305
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-05 16:28:35 -0300

    Faz todos os imports que serão necessários

---
+++ src/services/smithService.ts
@@ -0,0 +1,4 @@
+import connection from '../models/connection';
+import SmithModel from '../models/smithModel';
+import IProduct from '../interfaces/productInterface';
+import IUser from '../interfaces/userInterface';
